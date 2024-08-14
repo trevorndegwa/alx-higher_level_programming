589e81ac3761e775e1ffafdd6a8cbbec4aca98b0
trevorndegwa <tntrende@gmail.com>
2024-08-14 15:41:05 +0300

    whiteboarding task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,22 +9,22 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *head;
-	listint_t *tail;
+	listint_t *slow;
+	listint_t *fast;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	head = list;
-	tail = list;
+	slow = list;
+	fast = list->next;
 
-	while (tail != NULL && tail->next != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
-		head = head->next;
-		tail = tail->next->next;
-
-		if (head == tail)
+		if (slow == fast)
 			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
