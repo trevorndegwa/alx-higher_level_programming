48f39610b7eb0c370e88348046df44c640368d8a
trevorndegwa <tntrende@gmail.com>
2024-08-16 20:10:22 +0300

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv) - 1
     total = 0
-    if num == 0:
+    if num <= 0:
         print(0, end="")
     else:
         for item in range(num):
