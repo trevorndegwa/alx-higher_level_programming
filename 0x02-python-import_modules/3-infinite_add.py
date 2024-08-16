e1b4244ef63d9b40417dbf2b8d3727d33266ce89
trevorndegwa <tntrende@gmail.com>
2024-08-16 20:16:27 +0300

    sum or args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,11 +2,8 @@
 
 if __name__ == "__main__":
     import sys
-    num = len(sys.argv) - 1
-    total = 0
-    if num <= 0:
-        print(0, end="")
-    else:
-        for item in range(num):
-            total += int(sys.argv[item + 1])
-    print("{}".format(total))
+    total_sum = 0
+
+    for arg in sys.argv[1:]:
+        total_sum += int(arg)
+    print(total_sum)
