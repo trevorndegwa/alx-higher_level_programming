a19aa9955d323918c5cbb4bc1cf3ec401e26fa37
trevorndegwa <tntrende@gmail.com>
2024-08-16 19:51:37 +0300

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if num == 0:
         print("0 arguments.")
     elif num == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(num))
     for i in range(num):
