86bb7aa8cb953ddac1084d041686c96b40ec4eda
trevorndegwa <tntrende@gmail.com>
2024-08-23 10:11:40 +0300

    replacing element in position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    '''Function replaces element in list without altering original'''
+    if (idx < 0) or (idx >= len(my_list)):
+        return my_list
+    else:
+        copy_list = my_list[:]
+        copy_list.insert(idx, element)
+        return copy_list
